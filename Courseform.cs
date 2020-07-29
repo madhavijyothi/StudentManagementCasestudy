8acaa7e58dd459ac745d301c800f5402af22d8ab
madhavijyothi <52990768+madhavijyothi@users.noreply.github.com>
2020-07-29 17:57:14 +0530

    Add files via upload

---
+++ Courseform.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntityLayer;
+using BAL;
+
+namespace StudentManagement
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        studentBAL bal = new studentBAL();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CourseEntity cs = new CourseEntity();
+            cs.Name = txtCourseName.Text;
+            cs.Fees = long.Parse(txtFee.Text);
+            cs.Duration = cbCourseDuration.SelectedItem.ToString();
+            try
+            {
+                var res = bal.InsertCourse(cs);
+                if (res > 0)
+                    MessageBox.Show("Course Inserted and CourseId = " + res);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
+}
